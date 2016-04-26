3ce28a1fce989622a9397a93c891de759c583e89
Mouad El Merchichi <mouad.elmerchichi@edu.hefr.ch>
2016-04-26 10:16:34 +0200

    adding enemy shots

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Comportement générique pour les méchants
+/// </summary>
+public class EnemyScript : MonoBehaviour
+{
+    private WeaponScript[] weapons;
+
+    void Awake()
+    {
+        // Récupération de toutes les armes de l'ennemi
+        weapons = GetComponentsInChildren<WeaponScript>();
+    }
+
+    void Update()
+    {
+        foreach (WeaponScript weapon in weapons)
+        {
+            // On fait tirer toutes les armes automatiquement
+            if (weapon != null && weapon.CanAttack)
+            {
+                weapon.Attack(true);
+            }
+        }
+    }
+}
