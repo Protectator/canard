e7ebd5202e32f5896bed5a1be0869d24b57d2c5f
Noémien Kocher <nkcr.je@gmail.com>
2016-04-26 15:33:01 +0200

    menu enhancement

---
+++ Assets/Scripts/StartHover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class StartHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	public Text theText;
+
+	public Texture2D cursorTexture;
+	public CursorMode cursorMode = CursorMode.Auto;
+	public Vector2 hotSpot = Vector2.zero;
+
+	public void OnPointerEnter(PointerEventData enventData)
+	{
+		theText.color = Color.white;
+		Cursor.SetCursor (cursorTexture, hotSpot, cursorMode);
+	}
+
+	public void OnPointerExit(PointerEventData eventData) 
+	{
+		theText.color = Color.black;
+		Cursor.SetCursor (null, Vector2.zero, cursorMode);
+	}
+
+}
