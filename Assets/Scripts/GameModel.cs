549162fd0ac9c4501c6c6e1e275883e81bad4d91
Noémien Kocher <nkcr.je@gmail.com>
2016-04-26 23:30:51 +0200

    le gros du score, aura besoin de refactoring

---
+++ Assets/Scripts/GameModel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameModel : MonoBehaviour {
+
+	public static int score = 10;
+	public static int nextCanneton = 0;
+
+}
--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -17,44 +17,46 @@ public class HealthScript : MonoBehaviour
     public bool isEnemy = true;
 
 	private Text textLife;
+	private Text textScore;
 
 	void Start() 
 	{
 		textLife = GameObject.Find("Vie chiffre").GetComponent<Text>();
 		textLife.text = hp.ToString ();
+		textScore = GameObject.Find("Score chiffre").GetComponent<Text>();
 	}
 
     void OnTriggerEnter2D(Collider2D collider)
     {
         // Est-ce un tir ?
         ShotScript shot = collider.gameObject.GetComponent<ShotScript>();
-        if (shot != null)
-        {
-            // Tir allié
-            if (shot.isEnemyShot != isEnemy)
-            {
-                hp -= shot.damage;
+		if (shot != null) {
+			// Tir allié
+			if (shot.isEnemyShot != isEnemy) {
+				hp -= shot.damage;
 
-                // Destruction du projectile
-                // On détruit toujours le gameObject associé
-                // sinon c'est le script qui serait détruit avec ""this""
-                Destroy(shot.gameObject);
+				// Destruction du projectile
+				// On détruit toujours le gameObject associé
+				// sinon c'est le script qui serait détruit avec ""this""
+				Destroy (shot.gameObject);
 
 				if (!isEnemy) {
 					textLife.text = hp.ToString ();
 				}
 
-                if (hp <= 0)
-                {
-                    // 'Splosion!
-                    SpecialEffectsHelper.Instance.Explosion(transform.position);
+				if (hp <= 0) {
+					// 'Splosion!
+					SpecialEffectsHelper.Instance.Explosion (transform.position);
 
-                    SoundEffectsHelper.Instance.MakeExplosionSound();
+					SoundEffectsHelper.Instance.MakeExplosionSound ();
 
-                    // Destruction !
-                    Destroy(gameObject);
-                }
-            }
-        }
+					// Destruction !
+					Destroy (gameObject);
+				}
+			}
+		} else if (collider.gameObject.name == "Caneton") 
+		{
+			textScore.text = (++GameModel.score).ToString();
+		}
     }
 }
